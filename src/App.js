f838773d6b60fc7ebffeac38c8d0f859ae10dccd
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2020-12-23 16:39:04 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,54 @@ import React from 'react';
 
 import {Grid} from '@material-ui/core';
 
-import {SearchBar, VideoDetail} from './components';
+import {SearchBar, VideoDetail, VideoList} from './components';
 
 import youtube from './api/youtube';
 
 class App extends React.Component {
+    state = {
+        videos: [],
+        selectedVideo: null
+    }
+
+    componentDidMount() {
+        this.handleSubmit('pdf generation with react and node');
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video});
+    }
+
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: 'AIzaSyCWfni-Sm6y5J8iYKQxLU_3Sd3uS5CMsAU',
+                q: searchTerm
+             }
+        });
+
+        this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
+    }
+
     render() {
+        const {selectedVideo, videos} = this.state;
         return (
-           <Grid justify="center" container spacing={16}>
+           <Grid justify="center" container spacing={10}>
                <Grid item xs={12}>
-                   <Grid container spacing={16}>
+                   <Grid container spacing={10}>
                        <Grid item xs={12}>
                            {/* SEARCH BAR */}
-                           <SearchBar />
+                           <SearchBar onFormSubmit={this.handleSubmit} />
                        </Grid>
                        <Grid item xs={8}>
                            {/* VIDEO DETAILS */}
-                           <VideoDetail />
+                           <VideoDetail video={selectedVideo} />
                        </Grid>
                        <Grid item xs={4}>
                            {/* VIDEO LIST */}
+                           <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
                        </Grid>
                    </Grid>
                </Grid>
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-       part: 'snippet',
-       maxResults: 5,
-       key: '[API KEY]'
-    }
+    baseURL: 'https://www.googleapis.com/youtube/v3'
 });
